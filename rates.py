2cda8b3b26bbc2825eb3abbfd66ee4f7a3a521cf
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-02-20 15:52:16 +0000

    multiple airports at once

--- rates.py
+++ rates.py
@@ -1,101 +1,137 @@
-import requests,time
+import json
+import requests
+import time
 from shapely.geometry import Point, Polygon
+import os
+import threading
 
+url = "https://data.vatsim.net/v3/vatsim-data.json"  # regenerates every 15s
 
-class Airport:
-	def __init__(self,boundry, elevation,icao):
-		self.boundry = Polygon(boundry)
-		self.elevation = elevation
-		self.icao = icao
-
-airport = [(50.074935640323474, 8.44810178631773),(49.97464929980614, 8.48257453416403),(49.9961902982015, 8.664163374453615),(50.08132315919908, 8.631389988683265)]
-
-#EGKK
-#airport = [(51.13719270370898, -0.21951900613886854),(51.141721140088784, -0.14012682199274115),(51.1792900203234, -0.153118270307562),(51.16897294200857, -0.22919041766212397)]
-#EGPH
-#airport = [(55.971760870855405, -3.3355042299469266),(55.93283621469401, -3.3263203472236804),(55.934951645546896, -3.3925816353185465),(55.96210544001433, -3.406829529098473)]
-#EGCC
-#airport = [(53.37328989679097, -2.3346548753120744),(53.32256468988526, -2.3196255728380852),(53.326592705048924, -2.2386985595166053),(53.39006997975631, -2.2504522447847246)]
-airport_poly = Polygon(airport)
-url = "https://data.vatsim.net/v3/vatsim-data.json" # regenerates every 15s
-
-payload={}
+payload = {}
 headers = {
-  'Accept': 'application/json'
+    'Accept': 'application/json'
 }
 
-on_ground = set()
-departed = set()
-hour_track = set()
-global_start = time.time()
-rolling_rate = 15 
-rolling_rate *= 60 
 HOUR = 3600
 
 
-with open("rates.csv","w")as f:
-	f.write(f"0,\n0,\n0,")
-while True:
-	start = time.time()
-	response = requests.request("GET", url, headers=headers, data=payload)
+class Airport:
+    def __init__(self, boundary, elevation, icao):
+        self.boundary = Polygon(boundary)
+        self.elevation = elevation
+        self.icao = icao
+        self.on_ground = set()
+        self.departed = set()
+        self.hour_track = set()
+        self.global_start = time.time()
+        self.rolling_rate = 15
+        self.rolling_rate *= 60
 
-	#print(response.text)
-	data = response.json()
+        self.filename = f"{time.time()}_{self.icao}_departure_rate.csv"
+        self.stop = False
 
-	pilots = data.get('pilots', [])
-	currently_on_ground = set()
-	for pilot in pilots:
-		callsign = pilot.get('callsign')
-		lat = pilot.get('latitude', None)
-		lon = pilot.get('longitude', None)
-		alt = pilot.get('altitude', None)
-		plan = pilot.get('flight_plan', None)
-		if plan:
-			dep = plan.get('departure', None)
-			rules = plan.get('flight_rules')
-		if lat is not None and lon is not None and rules is not None and dep is not None:
-			point = Point(lat, lon)
-		if airport_poly.contains(point) and alt < 500 and dep == "EDDF" and rules == "I":
-			currently_on_ground.add(callsign)
-			print(f"Pilot {callsign} is within the airport polygon.")
+        with open(self.filename, "w") as f:
+            f.write(f"0,\n0,\n0,")
 
-	for cs in on_ground:
-		if cs not in currently_on_ground:
-			for pilot in pilots:
-				callsign = pilot.get('callsign') # TODO check alt + speed?
-				if callsign == cs: # still connected
-					dep_time = time.time()
-					departed.add((cs,dep_time))
-					hour_track.add((cs,dep_time))
-					print(f"{callsign} has departed!")
-					break
-	on_ground = currently_on_ground
+    def get_pilots(self):
+        response = requests.request(
+            "GET", url, headers=headers, data=payload)
 
-	hour_track = {(plane, tot) for plane, tot in hour_track if end - tot <= HOUR}
-	departed = {(plane, tot) for plane, tot in departed if end - tot <= rolling_rate}
+        return response.json().get('pilots', [])
 
-	departure_rate_per_hour = len(departed) * HOUR / rolling_rate
-	print(f"Corrected Departure rate: {departure_rate_per_hour:.2f} per hour")
-	print(f"Actual Departure Rate {len(hour_track)}")
+    def process_data(self):
+        while not self.stop:
+            start = time.time()
+            data = self.get_pilots()
+            currently_on_ground = set()
 
-	with open("rates.csv", "r")as f:
-		timestamps,dep_rates,hour_rates = f.read().splitlines()
-	timestamps += f"{time.time()},"
-	dep_rates += f"{departure_rate_per_hour},"
-	hour_rates += f"{len(hour_track)},"
-	
-	lines = f"{timestamps}\n{dep_rates}\n{hour_rates}"
+            for pilot in data:
+                callsign = pilot.get('callsign')
+                lat = pilot.get('latitude', None)
+                lon = pilot.get('longitude', None)
+                alt = pilot.get('altitude', None)
+                plan = pilot.get('flight_plan', None)
+
+                if plan:
+                    dep = plan.get('departure', None)
+                    rules = plan.get('flight_rules', None)
+                    if lat and lon and rules == "I" and dep == self.icao:
+                        point = Point(lat, lon)
+                        if self.boundary.contains(point) and alt < self.elevation:
+                            currently_on_ground.add(callsign)
+                            print(
+                                f"Pilot {callsign} is on the ground at {self.icao}")
+
+            for cs in self.on_ground:
+                if cs not in currently_on_ground:  # i.e they left
+                    for pilot in data:
+                        callsign = pilot.get('callsign', None)
+                        if callsign == cs:  # still connected
+                            departure_time = time.time()
+                            self.departed.add((cs, departure_time))
+                            self.hour_track.add((cs, departure_time))
+                            print(f"{callsign} has departed from {self.icao}")
+                            break
+
+            self.on_ground = currently_on_ground
+            self.departed = {(plane, tot) for plane, tot in self.departed if time.time(
+            ) - tot <= self.rolling_rate}
+            self.hour_track = {
+                (plane, tot) for plane, tot in self.hour_track if time.time() - tot <= HOUR}
+
+            estimated_rate = len(self.departed) * (HOUR / self.rolling_rate)
+            print(f"{self.icao} estimated departure rate: {estimated_rate} per hour")
+            print(
+                f"{self.icao} Current actual departure rate {len(self.hour_track)} per hour")
+
+            with open(self.filename, "r")as f:
+                timestamps, dep_rates, hour_rates = f.read().splitlines()
+                timestamps += f"{int(time.time())},"
+                dep_rates += f"{estimated_rate},"
+                hour_rates += f"{len(self.hour_track)},"
+
+                lines = f"{timestamps}\n{dep_rates}\n{hour_rates}"
+
+            with open(self.filename, "w")as f:
+                f.write(lines)
+
+            diff = time.time() - start
+            print(f"{self.icao} loop took {diff}s")
+            time.sleep(max(15-diff, 0))
 
 
-	
-	with open("rates.csv","w")as f:
-		f.write(lines)
+if __name__ == "__main__":
+    with open("boundingBoxes.json")as f:
+        airport_data = json.load(f)
 
+    tracking = {}
 
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-	end = time.time()
-	diff = end - start
-	print(f"Loop took {diff}")
-	time.sleep(max((15 - diff),0))
+    while True:
+        if tracking:
+            print(f"Tracking {', '.join(list(tracking))}")
+        airport = input("Enter airport ICAO to track\n> ")
+        if airport.upper() in tracking:
+            tracking[airport.upper()][1].stop = True
+            tracking[airport.upper()][0].join()
+            tracking.pop(airport.upper())
+            print(f"Stopped tracking {airport.upper()} ")
 
-		
+        elif airport.upper() in airport_data.keys():
+            bbox = airport_data.get(airport.upper()).get('boundingBox')
+            elev = airport_data.get(airport.upper()).get('elevation')
+            airport_tracker = Airport(bbox, elev, airport.upper())
+
+            thread = threading.Thread(target=airport_tracker.process_data)
+            thread.daemon = True
+            thread.start()
+
+            tracking[airport.upper()] = (thread, airport_tracker)
+            print(f"Now tracking {airport}")
+
+        else:
+            print("That airport is not in the datafile")
+
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
